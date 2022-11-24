d93c62d1e0c32f21f6797c2192aab4af293c81e7
Rafael Arantes <rafael-fullstack@outlook.com>
2022-11-24 10:17:22 -0300

    Add project files.

---
+++ Exception/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Excecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
